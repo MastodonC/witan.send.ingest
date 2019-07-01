c0ef0632849db7cd0d647073d3849a6580ed6b68
Bruce Durling <bruce@mastodonc.com>
2019-07-01 17:36:40 +0100

    Output valid-states to csv

---
+++ src/witan/send/ingest/valid_states.clj
@@ -0,0 +1,16 @@
+(ns witan.send.ingest.valid-states
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(defn ->csv [output-prefix valid-states]
+  (with-open [w (io/writer (str output-prefix "valid-states.csv"))]
+    (let [header [:setting :setting-group :min-academic-year :max-academic-year :needs :setting->setting]]
+      (csv/write-csv
+       w
+       (into [(mapv name header)]
+             (comp
+              (map (fn [m] (update m :needs #(s/join "," %))))
+              (map (fn [m] (update m :setting->setting #(s/join "," %))))
+              (map (apply juxt header)))
+             valid-states)))))
